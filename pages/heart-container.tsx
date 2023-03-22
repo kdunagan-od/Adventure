73f9185e155f45c42791b4786d72c3f88424b30a
Kim Dunagan <kdunagan@overdrive.com>
2023-03-22 18:08:56 -0400

    Create heart and heart container

---
+++ pages/heart-container.tsx
@@ -0,0 +1,13 @@
+import Heart from "./heart";
+
+export default function HeartContainer() {
+  return (
+    <div className='heart-container'>
+      <Heart></Heart>
+      <Heart></Heart>
+      <Heart></Heart>
+      <Heart></Heart>
+      <Heart></Heart>
+    </div>
+  )
+}
---
+++ pages/heart.tsx
@@ -0,0 +1,5 @@
+export default function Heart() {
+  return (
+    <div className='heart'>♥</div>
+  )
+}
